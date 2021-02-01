d9054e7b28cdcb2b5eae27142d35e87423731341
Valerio Carruba <48532340+valeriocarruba@users.noreply.github.com>
2021-02-01 11:16:42 -0300

    Batch update

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1054},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-objectives","displayName":"Objectives","link":{"linkType":"LinkTypePage","href":"#!page-objectives"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-useful-links","displayName":"Useful links","link":{"linkType":"LinkTypePage","href":"#!page-useful-links"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-references","displayName":"References","link":{"linkType":"LinkTypePage","href":"#!page-references"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+/* slider buttons */
+$(function() {
+    try {
+        var slider = $('.slider .unslider').children().get(0);
+        var unslider = $(slider).data('unslider');
+        $('.sub-nav').click(function() {
+            unslider.next()
+            $('.sub-nav-active').removeClass('sub-nav-active');
+            $(this).addClass('sub-nav-active');
+        });
+    }
+    catch(e) {
+    }
+})
